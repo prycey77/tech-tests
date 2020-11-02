a74bc5e439b68fff9490fc795ce903b74ad1918b
Richard <rich_pryce@yahoo.co.uk>
2020-11-02 11:46:44 +0000

    Balance decreases by amount withdrawn

--- lib/account.rb
+++ lib/account.rb
@@ -7,4 +7,7 @@ class Account
   def deposit(amount)
     @balance += amount
   end
+  def withdraw(amount)
+    @balance -= amount
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,11 +1,17 @@
 require 'account.rb'
 
 describe Account do
-  account = Account.new
-  account.deposit(1000)
-    it 'increases the balance by the amount deposited' do
-      expect(account.balance).to eq(1000)
+  it 'increases the balance by the amount deposited' do
+    account = Account.new
+    account.deposit(1000)
+    expect(account.balance).to eq(1000)
     end
 
+    it 'decreases the balance by the amount withdrawn' do
+      account = Account.new
+      account.deposit(1000)
+      account.withdraw(500)
+      expect(account.balance).to eq(500)
+    end
 
 end
