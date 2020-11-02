ef9abc4d44217fd7934636c60ce7f676744d0745
Richard <rich_pryce@yahoo.co.uk>
2020-11-02 11:54:38 +0000

    Refactored Account test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,17 +1,20 @@
 require 'account.rb'
 
 describe Account do
+  before(:each) do
+    @account = Account.new
+  end
+  
   it 'increases the balance by the amount deposited' do
-    account = Account.new
-    account.deposit(1000)
-    expect(account.balance).to eq(1000)
+    
+    @account.deposit(1000)
+    expect(@account.balance).to eq(1000)
     end
 
     it 'decreases the balance by the amount withdrawn' do
-      account = Account.new
-      account.deposit(1000)
-      account.withdraw(500)
-      expect(account.balance).to eq(500)
+      @account.deposit(1000)
+      @account.withdraw(500)
+      expect(@account.balance).to eq(500)
     end
 
 end
