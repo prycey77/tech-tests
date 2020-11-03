f3465ed04cd7e876e77090d9caae3eca56d67a16
Richard <rich_pryce@yahoo.co.uk>
2020-11-03 17:41:36 +0000

    Added Timecop and adjusted Transaction tests

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Transactions
-  attr_accessor :deposit, :debit, :date, :type
+  attr_accessor :deposit, :debit, :date
 
   def initialize(deposit, debit, date)
     @deposit = deposit
