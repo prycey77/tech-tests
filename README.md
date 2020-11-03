2116484909a1e121e8589f342c7ef9ca12251dbe
Richard <rich_pryce@yahoo.co.uk>
2020-11-03 12:39:21 +0000

    Print formatting corrected

--- README.md
+++ README.md
@@ -54,6 +54,7 @@ And a deposit of 2000 on 13-01-2012
 And a withdrawal of 500 on 14-01-2012
 When she prints her bank statement
 Then she would see
+
 ```
 date || credit || debit || balance
 14/01/2012 || || 500.00 || 2500.00
--- lib/printer.rb
+++ lib/printer.rb
@@ -2,13 +2,13 @@
 
 class Printer
   def print(history)
-    statement = "date    || credit || debit || balance\n"
+    statement = "date || credit || debit || balance\n"
     history.reverse.each do |transaction|
       statement += transaction.date + ' || '
       statement += if transaction.type == true
-                      '%.2f' % transaction.amount+ ' ||  || '
+                      '%.2f' % transaction.amount+ ' || || '
                    else
-                      ' ||   || ' + '%.2f' % transaction.amount + ' || '
+                      ' || || ' + '%.2f' % transaction.amount + ' || '
                    end
       puts statement += '%.2f' % transaction.balance + "\n"
     end
